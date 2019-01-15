cec85204e59cc8d49d43def6dc8e1fa5147d428f
phsantana <ph_santana@outlook.com>
2019-01-15 19:08:33 -0200

    Arquivos enviados

---
+++ js/parallax.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll",parallax);
+
+function parallax(){
+	[].forEach.call(document.querySelectorAll(".parallax"),parallax => {
+		var roll = -(parallax.getBoundingClientRect().top/5);
+		var bg = "calc(50% + " + roll + "px)";
+		parallax.style.backgroundPosition = 'center ' + bg;
+	});
+}
